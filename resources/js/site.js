945fd64a3379fb33ed63ef49e300fb237fa048df
Jason Baciulis <jason.baciulis@gmail.com>
2024-11-05 19:02:37 +0700

    banner

--- resources/js/site.js
+++ resources/js/site.js
@@ -2,7 +2,10 @@ import Alpine from 'alpinejs'
 import collapse from '@alpinejs/collapse'
 import persist from '@alpinejs/persist'
 import focus from '@alpinejs/focus'
+import Cookies from 'js-cookie'
 
+window.Cookies = Cookies
 window.Alpine = Alpine
+
 Alpine.plugin([collapse, focus, persist])
 Alpine.start()
