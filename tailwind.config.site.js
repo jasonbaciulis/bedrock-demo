9a585d639f05e230c6a24ed0b7cb2bbf2511c248
Jason Baciulis <jason.baciulis@gmail.com>
2024-11-11 15:45:35 +0700

    update fonts

--- tailwind.config.site.js
+++ tailwind.config.site.js
@@ -63,7 +63,7 @@ module.exports = {
       sans: [
         // Use a custom sans serif font for this site by changing 'Gaultier' to the
         // font name you want and uncommenting the following line.
-        'Inter',
+        'Mona Sans',
         ...defaultTheme.fontFamily.sans,
       ],
       serif: [
@@ -82,10 +82,10 @@ module.exports = {
       lg: ['1.125rem', { lineHeight: '1.75rem' }],
       xl: ['1.25rem', { lineHeight: '1.75rem' }],
       '2xl': ['1.5rem', { lineHeight: '2rem' }],
-      '3xl': ['1.875rem', { lineHeight: '2.25rem' }],
-      '4xl': ['2.25rem', { lineHeight: '2.5rem' }],
-      '5xl': ['3rem', { lineHeight: '1' }],
-      '6xl': ['3.75rem', { lineHeight: '1' }],
+      '3xl': ['1.875rem', { lineHeight: '1.2' }],
+      '4xl': ['2.25rem', { lineHeight: '1.2' }],
+      '5xl': ['3rem', { lineHeight: '1.2' }],
+      '6xl': ['3.75rem', { lineHeight: '1.2' }],
       '7xl': ['4.5rem', { lineHeight: '1' }],
       '8xl': ['6rem', { lineHeight: '1' }],
       '9xl': ['8rem', { lineHeight: '1' }],
