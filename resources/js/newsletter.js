c0f257a08bf0543bfc1fcc57466802c608a0f0ac
Jason Baciulis <jason.baciulis@gmail.com>
2024-11-14 17:08:11 +0700

    refactor

--- resources/js/newsletter.js
+++ resources/js/newsletter.js
@@ -7,15 +7,6 @@ document.addEventListener('alpine:init', () => {
       success: false,
       subscribed: false,
       email: '',
-      shake: false,
-
-      runShake() {
-        this.shake = true
-
-        setTimeout(() => {
-          this.shake = false
-        }, 200)
-      },
 
       isSubscribed() {
         const subscribed = localStorage.getItem(`${siteName}_newsletter_subscribed`)
@@ -23,48 +14,46 @@ document.addEventListener('alpine:init', () => {
       },
 
       async submit() {
-        // If hidden field is filled by bots show "successful" submission.
-        if (this.$refs.honeypot.value) {
-          this.errors = []
-          this.success = true
-          this.error = false
-          this.$el.reset()
+        try {
+          // If hidden field is filled by bots show "successful" submission.
+          if (this.$refs.honeypot.value) {
+            this.errors = []
+            this.success = true
+            this.error = false
+            this.$refs.form.reset()
 
-          return
+            return
+          }
+
+          this.sending = true
+
+          const response = await fetch(route, {
+            headers: {
+              'X-Requested-With': 'XMLHttpRequest',
+            },
+            method: 'POST',
+            body: new FormData(this.$refs.form),
+          })
+          const json = await response.json()
+
+          if (json['success']) {
+            this.errors = []
+            this.success = true
+            this.error = false
+
+            localStorage.setItem(`${siteName}_newsletter_subscribed`, true)
+
+            this.$refs.form.reset()
+          } else {
+            this.error = true
+            this.success = false
+            this.errors = json['errors']
+          }
+        } catch (error) {
+          console.error(err)
+        } finally {
+          this.sending = false
         }
-
-        this.sending = true
-
-        fetch(route, {
-          headers: {
-            'X-Requested-With': 'XMLHttpRequest',
-          },
-          method: 'POST',
-          body: new FormData(this.$refs.form),
-        })
-          .then(res => res.json())
-          .then(json => {
-            this.sending = false
-
-            if (json['success']) {
-              this.errors = []
-              this.success = true
-              this.error = false
-
-              localStorage.setItem(`${siteName}_newsletter_subscribed`, true)
-
-              this.$refs.form.reset()
-            } else {
-              this.error = true
-              this.success = false
-              this.errors = json['error']
-              this.runShake()
-            }
-          })
-          .catch(error => {
-            this.sending = false
-            this.runShake()
-          })
       },
     }
   })
