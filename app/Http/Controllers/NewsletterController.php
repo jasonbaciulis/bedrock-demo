d2491ae82693690d68dbc7cb8c6284f7eab86aaf
Jason Baciulis <jason.baciulis@gmail.com>
2024-11-14 17:12:46 +0700

    refactor to use provider indepedant

--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Exception;
-use MailchimpMarketing\ApiClient;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -19,36 +18,23 @@ class NewsletterController extends Controller
     {
         $request->validate([
             'email' => ['required', 'email:rfc,dns'],
-            'consent' => ['required', 'accepted'],
         ]);
-        $list = config('services.mailchimp.lists.newsletter');
 
         try {
-            $this->client()->lists->addListMember($list, [
-                'email_address' => $request->email,
-                'status' => 'subscribed',
-            ]);
+            // Your newsletter logic here
         } catch (Exception $e) {
             return response()->json(
                 [
-                    'message' => json_decode($e->getMessage())->message,
+                    'errors' => [
+                        'email' => [$e->getMessage()],
+                    ],
                 ],
-                Response::HTTP_UNPROCESSABLE_ENTITY
+                Response::HTTP_UNPROCESSABLE_ENTITY,
             );
         }
 
         return response()->json([
-            'result' => 'success',
-        ]);
-    }
-
-    protected function client(): ApiClient
-    {
-        return (new ApiClient())->setConfig([
-            'apiKey' => config('services.mailchimp.key'),
-            // To find the value for the server param log into your Mailchimp account and look at the URL in your browser.
-            // You’ll see something like https://us19.admin.mailchimp.com/; the us19 part is what you need.
-            'server' => 'us16',
+            'success' => true,
         ]);
     }
 }
