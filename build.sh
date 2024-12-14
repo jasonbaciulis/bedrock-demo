6c8d5e003504de15151b4d705ace34e270995c92
Jason Baciulis <jason.baciulis@gmail.com>
2024-12-14 13:13:08 +0200

    update

--- build.sh
+++ build.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
-# Install PHP & WGET
-yum install -y wget
-yum install -y php php-common php-curl php-mbstring php-gd php-gettext php-bcmath php-json php-xml php-fpm php-intl php-zip php-imap
+# Update package list and install prerequisites
+apt-get update -y
+apt-get install -y wget gnupg2 software-properties-common
+
+# Add PHP repository and install PHP 8.2
+add-apt-repository ppa:ondrej/php -y
+apt-get update -y
+apt-get install -y php8.2 php8.2-common php8.2-curl php8.2-mbstring php8.2-gd php8.2-gettext php8.2-bcmath php8.2-json php8.2-xml php8.2-fpm php8.2-intl php8.2-zip php8.2-imap
 
 # INSTALL COMPOSER
 EXPECTED_CHECKSUM="$(wget -q -O - https://composer.github.io/installer.sig)"
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+php8.2 -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_CHECKSUM="$(php8.2 -r "echo hash_file('sha384', 'composer-setup.php');")"
 
 if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
 then
@@ -16,14 +21,14 @@ then
     exit 1
 fi
 
-php composer-setup.php --quiet
+php8.2 composer-setup.php --quiet
 rm composer-setup.php
 
 # INSTALL COMPOSER DEPENDENCIES
-php composer.phar install
+php8.2 composer.phar install
 
 # GENERATE APP KEY
-php artisan key:generate
+php8.2 artisan key:generate
 
 # BUILD STATIC SITE
-php please ssg:generate
+php8.2 please ssg:generate
