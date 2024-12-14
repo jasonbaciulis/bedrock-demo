c8931190f8bafd2f574d71fe633692bffdc6d892
Jason Baciulis <jason.baciulis@gmail.com>
2024-12-14 13:07:06 +0200

    update build script

--- build.sh
+++ build.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
 # Install PHP & WGET
-yum install -y amazon-linux-extras
-amazon-linux-extras enable php8.2
-yum clean metadata
-yum install php php-{common,curl,mbstring,gd,gettext,bcmath,json,xml,fpm,intl,zip,imap}
-yum install wget
+yum install -y wget
+yum install -y php php-common php-curl php-mbstring php-gd php-gettext php-bcmath php-json php-xml php-fpm php-intl php-zip php-imap
 
 # INSTALL COMPOSER
 EXPECTED_CHECKSUM="$(wget -q -O - https://composer.github.io/installer.sig)"
