a2a38213806aa0e878b29c553aa5cc66b46c059f
Jason Baciulis <jason.baciulis@gmail.com>
2025-08-21 15:16:06 +0700

    set up precognition

--- resources/js/components/form.js
+++ resources/js/components/form.js
@@ -1,80 +1,31 @@
 document.addEventListener('alpine:init', () => {
-  Alpine.data('form', ({ handle }) => {
+  Alpine.data('form', () => {
     return {
       error: false,
-      errors: [],
-      sending: false,
       success: false,
 
       submit() {
-        this.runFetch(this.$refs.form.action, this[handle])
+        this.form
+          .submit()
+          .then(response => {
+            console.log(response)
+            this.handleSuccess()
+          })
+          .catch(error => {
+            console.log(error)
+            this.handleError()
+          })
       },
 
-      async runFetch(route, data, callback = () => {}) {
-        this.sending = true
-
-        try {
-          const response = await this.fetchData(route, data)
-          const json = await response.json()
-
-          if (json['success']) {
-            this.handleSuccess(json, callback)
-          }
-
-          if (json['error']) {
-            this.handleError(json)
-          }
-        } catch (err) {
-          console.error(err)
-        } finally {
-          this.sending = false
-          this.$refs.form.scrollIntoView()
-        }
-      },
-
-      async fetchData(route, data) {
-        return await fetch(route, {
-          headers: {
-            'X-Requested-With': 'XMLHttpRequest',
-            'Content-Type': 'application/json',
-            'X-CSRF-TOKEN': this.$refs.form._token.value,
-          },
-          method: 'POST',
-          body: JSON.stringify(data),
-        })
-      },
-
-      handleSuccess(json, callback) {
-        this.errors = []
+      handleSuccess() {
         this.success = true
         this.error = false
-        this.$refs.form.reset()
-
-        callback(json, this)
+        this.form.reset()
       },
 
-      handleError(json) {
+      handleError() {
         this.error = true
         this.success = false
-        this.errors = json['error']
-      },
-
-      hasError(name) {
-        return this.errors.hasOwnProperty(name)
-      },
-
-      forgetError(name) {
-        const newErrors = { ...this.errors }
-        delete newErrors[name]
-        this.errors = newErrors
-      },
-
-      setError(object) {
-        const newErrors = { ...this.errors }
-        Object.keys(object).forEach(key => {
-          newErrors[key] = object[key]
-        })
-        this.errors = newErrors
       },
     }
   })
--- resources/js/site.js
+++ resources/js/site.js
@@ -1,13 +1,14 @@
 import Alpine from 'alpinejs'
-import collapse from '@alpinejs/collapse'
-import persist from '@alpinejs/persist'
-import focus from '@alpinejs/focus'
-import intersect from '@alpinejs/intersect'
-import mask from '@alpinejs/mask'
+import Collapse from '@alpinejs/collapse'
+import Persist from '@alpinejs/persist'
+import Focus from '@alpinejs/focus'
+import Intersect from '@alpinejs/intersect'
+import Mask from '@alpinejs/mask'
+import Precognition from 'laravel-precognition-alpine'
 import Cookies from 'js-cookie'
 
 window.Cookies = Cookies
 window.Alpine = Alpine
 
-Alpine.plugin([collapse, focus, persist, intersect, mask])
+Alpine.plugin([Collapse, Focus, Persist, Intersect, Mask, Precognition])
 Alpine.start()
