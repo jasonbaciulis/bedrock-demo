8346d86b10af9928c22dc3da0e61ccab9353cff3
Jason Baciulis <jason.baciulis@gmail.com>
2025-08-21 15:18:10 +0700

    use request

--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -2,8 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Exception;
-use Illuminate\Http\Request;
+use App\Http\Requests\NewsletterRequest;
 use Symfony\Component\HttpFoundation\Response;
 
 class NewsletterController extends Controller
@@ -14,27 +13,15 @@ class NewsletterController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public function __invoke(Request $request)
+    public function __invoke(NewsletterRequest $request)
     {
-        $request->validate([
-            'email' => ['required', 'email:rfc,dns'],
-        ]);
+        // Your newsletter logic here
 
-        try {
-            // Your newsletter logic here
-        } catch (Exception $e) {
-            return response()->json(
-                [
-                    'errors' => [
-                        'email' => [$e->getMessage()],
-                    ],
-                ],
-                Response::HTTP_UNPROCESSABLE_ENTITY,
-            );
-        }
-
-        return response()->json([
-            'success' => true,
-        ]);
+        return response()->json(
+            [
+                'success' => true,
+            ],
+            Response::HTTP_OK
+        );
     }
 }
