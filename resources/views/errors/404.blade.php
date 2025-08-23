336a1f1b04d3b7058ec9b2cbd52b7776a49aa1e9
Jason Baciulis <jason.baciulis@gmail.com>
2025-08-23 12:38:39 +0700

    fix 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,15 +1,19 @@
-<main id="content">
-    <section class="m-section">
-        <div class="container site-grid">
-            <div class="col-span-full lg:col-span-6">
-                <h1 class="h1">
-                    404 Error
-                </h1>
-                <p class="mt-3 content-xl">The page you're looking for does not exist.</p>
-                <div class="mt-8">
-                    <a href="{{ site:url }}" class="btn btn--primary btn--lg">Go to homepage</a>
+@extends('layout')
+
+@section('body')
+    <main id="content">
+        <section class="m-section">
+            <div class="container site-grid">
+                <div class="col-span-full lg:col-span-6">
+                    <h1 class="h1">
+                        404 Error
+                    </h1>
+                    <p class="mt-3 content-xl">The page you're looking for does not exist.</p>
+                    <div class="mt-8">
+                        <a href="{{ $site->url }}" class="btn btn--primary btn--lg">Go to homepage</a>
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
-</main>
+        </section>
+    </main>
+@endsection
