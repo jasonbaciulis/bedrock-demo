bf423d3a0700084ffc8f95742631ae55634e973f
Jason Baciulis <jason.baciulis@gmail.com>
2025-08-24 18:28:49 +0700

    convert

--- resources/views/blocks/team.blade.php
+++ resources/views/blocks/team.blade.php
@@ -1,26 +1,30 @@
-<section class="m-section">
+<section id="{{ Statamic::modify($block->type)->slugify() }}" class="m-section">
     <div class="container">
-        {{ partial:partials/section-header }}
+        @include('partials.section-header', [
+            'title' => $block->title,
+            'text' => $block->text ?? null,
+        ])
 
-        {{ if query === 'custom' }}
-            {{ $entries = entries }}
-        {{ elseif query === 'ordered' }}
-            {{ $entries = {collection:team :limit="limit" sort="order"} }}
-        {{ elseif query === 'featured' }}
-            {{ $entries = {collection:team :limit="limit" featured:is="true" sort="order"} }}
-        {{ /if }}
+        @if ($block->query->value() === 'custom')
+            @php($entries = $block->entries)
+        @elseif ($block->query->value() === 'ordered')
+            @php($entries = Statamic::tag('collection:team')->limit($block->limit)->sort('order')->fetch())
+        @elseif ($block->query->value() === 'featured')
+            @php($entries = Statamic::tag('collection:team')->limit($block->limit)->featured()->sort('order')->fetch())
+        @endif
 
         <div class="site-grid gap-y-20">
-            {{ $entries }}
+            @php($entries = Statamic::tag('collection:team')->limit($block->limit)->sort('order')->fetch())
+            @foreach ($entries as $entry)
                 <div class="sm:col-span-6 lg:col-span-4">
                     <div class="aspect-3/2 rounded-2xl bg-gray-100 ring-1 ring-gray-900/10 ring-inset">
-                        {{ partial:components/ui/picture w="384" h="256" cover="true" class="rounded-2xl" }}
+                        <x-ui.picture :image="$entry->image" w="384" h="256" cover="true" class="rounded-2xl" />
                     </div>
-                    <h3 class="h5 mt-6">{{ title }}</h3>
-                    <p class="text-muted-foreground">{{ position }}</p>
-                    <p class="mt-4 text-muted-foreground">{{ bio }}</p>
+                    <h3 class="h5 mt-6">{!! $entry->title !!}</h3>
+                    <p class="text-muted-foreground">{!! $entry->position !!}</p>
+                    <p class="mt-4 text-muted-foreground">{!! $entry->bio !!}</p>
                 </div>
-            {{ /$entries }}
+            @endforeach
         </div>
     </div>
 </section>
--- resources/views/components/ui/picture.blade.php
+++ resources/views/components/ui/picture.blade.php
@@ -1,64 +1,62 @@
-{{#
+{{--
     Lightweight picture component that uses Glide to generate images. It supports lazy loading and object-cover.
     Notice that the component doesn't use sizes attribute, because in our view it's a waste of time.
     Just pass the dimensions of the largest image size that needs rendered. Images are lazy loaded and saving a few kilobytes
     on the initial load is not worth the extra complexity.
 
     Docs: https://statamic.dev/tags/glide
+--}}
 
-    Params:
-    - w: Width of the image.
-    - h: Height of the image.
-    - class: Additional classes for the image.
-    - cover: Whether to use object-cover. Defaults to `false`.
-    - not_lazy: Whether to disable lazy loading. Defaults to `false`.
- #}}
+@props([
+    'image' => null,
+    'w' => null,
+    'h' => null,
+    'cover' => false,
+    'lazy' => true,
+])
 
-{{ if image }}
+@if ($asset = Statamic::tag('asset')->src($image)->fetch())
     <picture>
-        {{ asset :url="image" }}
-            {{ if extension === 'svg' || extension === 'gif' }}
-                <img
-                    {{ if cover }}
-                        class="object-cover size-full {{ class }}"
-                        style="object-position: {{ focus | background_position }}"
-                    {{ else }}
-                        {{ class | attribute:class }}
-                    {{ /if }}
-                    src="{{ url }}"
-                    alt="{{ alt }}"
-                    {{ unless not_lazy }}
-                        loading="lazy"
-                    {{ /unless }}
-                    decoding="async"
-                    width="{{ width }}"
-                    height="{{ height }}"
-                />
-            {{ elseif w && h }}
-                <source srcset="
-                    {{ glide:url :width='w' :height='h' fit='crop_focal' format='webp' }} 1x,
-                    {{ glide:url :width='w' :height='h' fit='crop_focal' format='webp' dpr='2' }} 2x" type="image/webp">
-                <source srcset="
-                    {{ glide:url :width='w' :height='h' fit='crop_focal' }} 1x,
-                    {{ glide:url :width='w' :height='h' fit='crop_focal' dpr='2' }} 2x" type="{{ image.mime_type }}">
-                <img {{ if cover }}
-                        class="object-cover size-full {{ class }}"
-                        style="object-position: {{ focus | background_position }}"
-                    {{ else }}
-                        {{ class | attribute:class }}
-                    {{ /if }}
-                    {{ glide :src="url" :width='w' :height='h' fit='crop_focal' }}
-                        src="{{ url }}"
-                        alt="{{ alt }}"
-                        width="{{ width }}"
-                        height="{{ height }}"
-                    {{ /glide }}
-                    {{ unless not_lazy }}
-                        loading="lazy"
-                    {{ /unless }}
-                    decoding="async"
-                >
-            {{ /if }}
-        {{ /asset }}
+        @if ($asset->extension() === 'svg' || $asset->extension() === 'gif')
+            <img
+                @if ($cover)
+                    {{ $attributes->class(['object-cover size-full']) }}
+                    style="object-position: {{ Statamic::modify($asset->focus)->backgroundPosition() }}"
+                @else
+                    {{ $attributes }}
+                @endif
+                src="{{ $asset->url }}"
+                alt="{{ $asset->alt }}"
+                @if ($lazy)
+                    loading="lazy"
+                @endif
+                decoding="async"
+                width="{{ $asset->width }}"
+                height="{{ $asset->height }}"
+            />
+        @elseif ($w && $h)
+            <source srcset="
+                {{ Statamic::tag('glide')->src($asset->url)->width($w)->height($h)->fit('crop_focal')->format('webp')->fetch() }} 1x,
+                {{ Statamic::tag('glide')->src($asset->url)->width($w)->height($h)->fit('crop_focal')->format('webp')->dpr('2')->fetch() }} 2x" type="image/webp">
+            <source srcset="
+                {{ Statamic::tag('glide')->src($asset->url)->width($w)->height($h)->fit('crop_focal')->fetch() }} 1x,
+                {{ Statamic::tag('glide')->src($asset->url)->width($w)->height($h)->fit('crop_focal')->dpr('2')->fetch() }} 2x" type="{{ $asset->mime_type }}">
+            <img
+                @if ($cover)
+                    {{ $attributes->class(['object-cover size-full']) }}
+                    style="object-position: {{ Statamic::modify($asset->focus)->backgroundPosition() }}"
+                @else
+                    {{ $attributes }}
+                @endif
+                    src="{{ $asset->url }}"
+                    alt="{{ $asset->alt }}"
+                    width="{{ $w }}"
+                    height="{{ $h }}"
+                @if ($lazy)
+                    loading="lazy"
+                @endif
+                decoding="async"
+            >
+        @endif
     </picture>
-{{ /if }}
+@endif
