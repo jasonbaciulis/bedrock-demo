6b03f54adec63025f1156934ef033bc5d1a07270
Jason Baciulis <jason.baciulis@gmail.com>
2025-08-25 11:07:34 +0700

    improve

--- resources/views/components/ui/collapsible.blade.php
+++ resources/views/components/ui/collapsible.blade.php
@@ -4,13 +4,23 @@
     'name' => Str::random(8),
 ])
 
-<div x-data="{ open: false }" {{ $attributes->class(['relative']) }}>
+<div
+    x-data="{
+        open: false,
+        toggle() { this.open = !this.open },
+        close() { this.open = false }
+    }"
+    {{ $attributes->class(['relative']) }}
+>
     <dt>
         <button
             type="button"
             aria-controls="collapsible-{{ $name }}"
             x-bind:aria-expanded="open"
-            x-on:click="open = !open"
+            x-on:click="toggle()"
+            x-on:keydown.enter.prevent="toggle()"
+            x-on:keydown.space.prevent="toggle()"
+            x-on:keydown.escape="close()"
             {{ $trigger->attributes }}
         >
             {{ $trigger }}
@@ -21,6 +31,8 @@
         x-show="open"
         x-collapse
         id="collapsible-{{ $name }}"
+        role="region"
+        x-bind:aria-hidden="!open"
         {{ $content->attributes->class(['relative']) }}
     >
         {{ $content }}
