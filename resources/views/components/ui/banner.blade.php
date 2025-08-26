2b5d20de5f0721360afaad0998b0962efa32d26a
Jason Baciulis <jason.baciulis@gmail.com>
2025-08-26 19:28:08 +0700

    convert

--- resources/views/components/ui/banner.blade.php
+++ resources/views/components/ui/banner.blade.php
@@ -1,7 +1,7 @@
-{{-- {{#
+{{--
     Configure Banner inside Control Panel in /cp/globals/banner
     Relies on a tiny `js-cookie` npm package for setting and getting cookies.
- #}}
+--}}
 
 <div
     x-cloak
@@ -19,21 +19,20 @@
         },
         dismiss() {
             this.visible = false;
-            Cookies.set(this.cookieName, true, { expires: {{ banner:cookie_expires }} });
+            Cookies.set(this.cookieName, true, { expires: {{ $banner->cookie_expires }} });
       },
     }"
     x-init="initBannerDisplay()"
     class="flex items-center gap-x-6 bg-foreground px-6 py-2.5 sm:px-3.5 sm:before:flex-1"
 >
     <div class="text-sm/6 text-white">
-        <a href="{{ banner:link_url }}">
-            {{ banner:text }}
+        <a href="{{ $banner->link_url }}">
+            {!! $banner->text !!}
         </a>
     </div>
     <div class="flex flex-1 justify-end">
-        <button type="button" class="btn -m-2 p-2 hover:opacity-80" @click="dismiss()">
-            <span class="sr-only">Dismiss</span>
-            {{ icon:lucide-x class="size-4 text-white" }}
+        <button type="button" class="btn -m-2 p-2 hover:opacity-80" x-on:click="dismiss()" aria-label="Dismiss">
+            <x-lucide-x class="size-4 text-white" />
         </button>
     </div>
-</div> --}}
+</div>
