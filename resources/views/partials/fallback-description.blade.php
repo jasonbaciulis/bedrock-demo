f5046fddaa81b8a984cee360dc0d0984fb36fcfd
Jason Baciulis <jason.baciulis@gmail.com>
2025-08-26 15:20:13 +0700

    fix fallbacks

--- resources/views/partials/fallback-description.blade.php
+++ resources/views/partials/fallback-description.blade.php
@@ -1,44 +1,19 @@
-@foreach (collect($seo->collection_defaults)->where('collection', $collection) as $item)
-    @if ($item['fallback'] == 'field')
-        @foreach ($item['field'] as $field)
-            {!! Statamic::modify($field['field_handle'])->stripTags()->entities()->trim()->safeTruncate(159, '…') !!}
-        @endforeach
-    @elseif ($item['fallback'] == 'custom_text')
-        {!! $item['custom_text'] !!}
-    @elseif ($item['fallback'] == 'blocks')
-        @if ($first_block = Arr::first($item['blocks'], fn($block) => $block['type'] == 'text'))
-            @if (is_array($first_block))
-                {!! Statamic::modify($first_block)
-                        ->raw()
-                        ->where('type', 'paragraph')
-                        ->bardText()
-                        ->stripTags()
-                        ->entities()
-                        ->trim()
-                        ->safeTruncate(159, '…')
-                !!}
-            @else
-                {!! Statamic::modify($first_block)
-                    ->stripTags()
-                    ->entities()
-                    ->trim()
-                    ->safeTruncate(159, '…')
-                !!}
-            @endif
+@if ($item = collect($seo->collection_defaults)->where('collection', $collection->value()->handle())->first())
+    @php($fallback = $item['fallback']->value())
+    @if ($fallback === 'field')
+        @php($field = $page->{$item['field_handle']})
+        @if (is_array($field))
+            {!! Statamic::modify($field)->bardText()->stripTags()->trim()->safeTruncate([160, '…']) !!}
         @else
-            @foreach ($item['blocks'] as $block)
-                @if ($block['type'] == 'article')
-                    {!! Statamic::modify($block['article'])
-                        ->raw()
-                        ->where('type', 'paragraph')
-                        ->bardText()
-                        ->stripTags()
-                        ->entities()
-                        ->trim()
-                        ->safeTruncate(159, '…')
-                    !!}
-                @endif
-            @endforeach
+            {!! Statamic::modify($field)->stripTags()->trim()->safeTruncate([160, '…']) !!}
+        @endif
+    @elseif ($fallback === 'custom_text')
+        {!! $item['custom_text'] !!}
+    @elseif ($fallback === 'blocks')
+        @if ($first_block = Arr::first($page->blocks)->all())
+            @unless (empty($first_block['text']))
+                {!! Statamic::modify($first_block['text'])->stripTags()->trim()->safeTruncate([160, '…']) !!}
+            @endunless
         @endif
     @endif
-@endforeach
+@endif
