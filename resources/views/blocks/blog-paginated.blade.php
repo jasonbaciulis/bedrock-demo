b2154c146bc355e88547f5d2310d799f53a87a3b
Jason Baciulis <jason.baciulis@gmail.com>
2025-08-26 21:26:28 +0700

    conditionals

--- resources/views/blocks/blog-paginated.blade.php
+++ resources/views/blocks/blog-paginated.blade.php
@@ -3,19 +3,21 @@
         <x-section-header :title="$block->title" :text="$block->text ?? null" />
 
         <s:collection from="posts" paginate="true" :limit="$block->limit" on_each_side="1" as="posts">
-            <div class="site-grid gap-y-20 md:gap-y-12">
-                @foreach ($posts as $post)
-                    <x-entry-posts
-                        :image="$post->image"
-                        :url="$post->url"
-                        :title="$post->title"
-                        :excerpt="$post->excerpt"
-                        :date="$post->date"
-                        :categories="$post->categories"
-                        class="col-span-full md:col-span-6 lg:col-span-4"
-                    />
-                @endforeach
-            </div>
+            @isset($posts)
+                <div class="site-grid gap-y-20 md:gap-y-12">
+                    @foreach ($posts as $post)
+                        <x-entry-posts
+                            :image="$post->image"
+                            :url="$post->url"
+                            :title="$post->title"
+                            :excerpt="$post->excerpt"
+                            :date="$post->date"
+                            :categories="$post->categories"
+                            class="col-span-full md:col-span-6 lg:col-span-4"
+                        />
+                    @endforeach
+                </div>
+            @endisset
 
             @isset($paginate)
                 <x-ui.pagination class="mt-20" :$paginate />
--- resources/views/blocks/search-results.blade.php
+++ resources/views/blocks/search-results.blade.php
@@ -7,27 +7,29 @@
             Docs: https://statamic.dev/search#indexes
         --}}
         <s:search:results index="blog" :limit="$block->limit" paginate="true" on_each_side="1" as="results">
-            <div class="site-grid gap-y-20 md:gap-y-12">
-                <div class="sm:col-span-12 max-w-md">
-                    <x-search-form :action="$theme->search_results->url" :total_results="isset($paginate) ? $paginate['total_items'] : 0" />
-                </div>
-
-                @forelse ($results as $result)
-                    <x-entry-posts
-                        :image="$result->image"
-                        :url="$result->url"
-                        :title="$result->title"
-                        :excerpt="$result->excerpt"
-                        :date="$result->date"
-                        :categories="$result->categories"
-                        class="col-span-full md:col-span-6 lg:col-span-4"
-                    />
-                @empty
-                    <div class="rounded-lg bg-yellow-50 sm:col-span-8 px-6 py-4">
-                        <p class="text-lg text-yellow-800">{!! $block->no_results_text !!}</p>
+            @isset($results)
+                <div class="site-grid gap-y-20 md:gap-y-12">
+                    <div class="sm:col-span-12 max-w-md">
+                        <x-search-form :action="$theme->search_results->url" :total_results="isset($paginate) ? $paginate['total_items'] : 0" />
                     </div>
-                @endforelse
-            </div>
+
+                    @forelse ($results as $result)
+                        <x-entry-posts
+                            :image="$result->image"
+                            :url="$result->url"
+                            :title="$result->title"
+                            :excerpt="$result->excerpt"
+                            :date="$result->date"
+                            :categories="$result->categories"
+                            class="col-span-full md:col-span-6 lg:col-span-4"
+                        />
+                    @empty
+                        <div class="rounded-lg bg-yellow-50 sm:col-span-8 px-6 py-4">
+                            <p class="text-lg text-yellow-800">{!! $block->no_results_text !!}</p>
+                        </div>
+                    @endforelse
+                </div>
+            @endisset
 
             @isset($paginate)
                 <x-ui.pagination class="mt-20" :$paginate />
