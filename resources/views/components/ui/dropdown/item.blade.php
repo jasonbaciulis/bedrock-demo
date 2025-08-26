cd1476aad4601e331b2545eac455f929df9b064c
Jason Baciulis <jason.baciulis@gmail.com>
2025-08-26 22:44:03 +0700

    fix item classes

--- resources/views/components/ui/dropdown/item.blade.php
+++ resources/views/components/ui/dropdown/item.blade.php
@@ -4,7 +4,7 @@
 
 <{{ $as }}
     {{ $attributes->class([
-        'hover:bg-accent hover:text-accent-foreground [&_svg:not([class*=\'text-\'])]:text-muted-foreground relative flex items-center gap-2 rounded-md px-2 py-1.5 text-sm outline-hidden select-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*=\'size-\'])]:size-4'
+        'hover:bg-accent hover:text-accent-foreground [&_svg:not([class*="text-"])]:text-muted-foreground relative flex items-center gap-2 rounded-md px-2 py-1.5 text-sm outline-hidden select-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*="size-"])]:size-4'
     ]) }}
 >
     {{ $slot }}
