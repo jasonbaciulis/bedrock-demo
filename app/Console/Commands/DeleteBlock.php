00574b6b22082c41cedf1ec77ac4a59ba7ef8fce
Jason Baciulis <jason.baciulis@gmail.com>
2025-08-28 15:30:29 +0700

    fix to handle blocks in any group

--- app/Console/Commands/DeleteBlock.php
+++ app/Console/Commands/DeleteBlock.php
@@ -26,7 +26,7 @@ class DeleteBlock extends Command
      *
      * @var string
      */
-    protected $description = 'Delete a page builder block.';
+    protected $description = 'Delete a Statamic page builder block.';
 
     /**
      * The block name.
@@ -117,14 +117,41 @@ class DeleteBlock extends Command
         $fieldsetPath = base_path('resources/fieldsets/blocks.yaml');
         $fieldset = Yaml::parseFile($fieldsetPath);
 
-        $existingGroups = Arr::get($fieldset, 'fields.0.field.sets.main.sets', []);
+        $existingGroups = Arr::get($fieldset, 'fields.0.field.sets', []);
 
-        if (Arr::exists($existingGroups, $this->fieldset_name)) {
-            Arr::forget($existingGroups, $this->fieldset_name);
+        // Find which group contains the block to delete
+        $groupKey = $this->findBlockGroup($existingGroups, $this->fieldset_name);
+
+        if (!$groupKey) {
+            $this->warn(
+                "Block '{$this->block_name}' not found in blocks.yaml - it may have already been removed."
+            );
+
+            return;
         }
 
-        Arr::set($fieldset, 'fields.0.field.sets.main.sets', $existingGroups);
+        // Remove the block from the found group
+        $groupSets = Arr::get($existingGroups, "{$groupKey}.sets", []);
+        if (Arr::exists($groupSets, $this->fieldset_name)) {
+            Arr::forget($groupSets, $this->fieldset_name);
+            Arr::set($existingGroups, "{$groupKey}.sets", $groupSets);
+            Arr::set($fieldset, 'fields.0.field.sets', $existingGroups);
+            File::put($fieldsetPath, Yaml::dump($fieldset, 99, 2));
+        }
+    }
 
-        File::put($fieldsetPath, Yaml::dump($fieldset, 99, 2));
+    /**
+     * Find which group contains the specified block.
+     */
+    protected function findBlockGroup(array $groups, string $blockName): ?string
+    {
+        foreach ($groups as $groupKey => $group) {
+            $groupSets = Arr::get($group, 'sets', []);
+            if (Arr::exists($groupSets, $blockName)) {
+                return $groupKey;
+            }
+        }
+
+        return null;
     }
 }
