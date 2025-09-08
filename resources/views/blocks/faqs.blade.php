0407e6174e0d2c39ad4d157a8e60b2080a838611
Jason Baciulis <jason.baciulis@gmail.com>
2025-09-08 22:26:29 +0700

    fix text

--- resources/views/blocks/faqs.blade.php
+++ resources/views/blocks/faqs.blade.php
@@ -1,6 +1,6 @@
 <section id="{{ Statamic::modify($block->type)->slugify() }}" class="m-section">
     <div class="container max-w-4xl">
-        <x-section-header :title="$block->title" :description="$block->description" />
+        <x-section-header :title="$block->title" :text="$block->text ?? null" />
 
         <x-faqs :items="$block->items" class="divide-y divide-gray-300 border-b border-gray-300">
             @foreach ($block->items as $item)
