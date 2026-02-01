46592609278ebb2d06570b714fdede4409554f99
Jason Baciulis <jason@remarkable.dev>
2026-02-01 14:02:01 +0200

    build php

--- build.sh
+++ build.sh
@@ -1,24 +1,63 @@
 #!/bin/sh
 set -e
 
-# Install required tools
-dnf install -y wget tar xz
+echo "=== Installing build dependencies ==="
+dnf install -y wget tar xz gcc make autoconf \
+    libxml2-devel openssl-devel sqlite-devel \
+    libcurl-devel oniguruma-devel libpng-devel \
+    libzip-devel libicu-devel
 
-# Download prebuilt PHP 8.4 binary from shivammathur/php-builder
+# Download and compile PHP 8.4
 PHP_VERSION="8.4.4"
-echo "Downloading PHP ${PHP_VERSION}..."
-wget -O php.tar.xz "https://github.com/shivammathur/php-builder/releases/download/php-${PHP_VERSION}/php-${PHP_VERSION}-linux-x64.tar.xz"
-mkdir -p php-bin
-tar -xf php.tar.xz -C php-bin
-PHP_BIN="$(pwd)/php-bin/bin/php"
-chmod +x $PHP_BIN
+echo "=== Downloading PHP ${PHP_VERSION} source ==="
+wget -O php.tar.xz "https://www.php.net/distributions/php-${PHP_VERSION}.tar.xz"
+tar -xf php.tar.xz
+cd "php-${PHP_VERSION}"
+
+echo "=== Configuring PHP ==="
+./configure \
+    --prefix=/usr/local \
+    --disable-all \
+    --enable-cli \
+    --enable-phar \
+    --enable-filter \
+    --enable-tokenizer \
+    --enable-ctype \
+    --enable-mbstring \
+    --enable-intl \
+    --enable-bcmath \
+    --enable-gd \
+    --with-zlib \
+    --with-openssl \
+    --with-curl \
+    --with-zip \
+    --enable-dom \
+    --enable-xml \
+    --enable-xmlreader \
+    --enable-xmlwriter \
+    --enable-simplexml \
+    --with-libxml \
+    --enable-pdo \
+    --with-pdo-sqlite \
+    --enable-fileinfo \
+    --enable-session \
+    --enable-posix \
+    --enable-pcntl \
+    2>&1
+
+echo "=== Compiling PHP (this will take a while) ==="
+make -j$(nproc) 2>&1
+make install 2>&1
+
+cd ..
+PHP_BIN="/usr/local/bin/php"
 
 # Verify PHP version
-echo "PHP version:"
+echo "=== PHP version ==="
 $PHP_BIN -v
 
 # INSTALL COMPOSER
-echo "Installing Composer..."
+echo "=== Installing Composer ==="
 EXPECTED_CHECKSUM="$(wget -q -O - https://composer.github.io/installer.sig)"
 $PHP_BIN -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 ACTUAL_CHECKSUM="$($PHP_BIN -r "echo hash_file('sha384', 'composer-setup.php');")"
@@ -34,15 +73,17 @@ $PHP_BIN composer-setup.php --quiet
 rm composer-setup.php
 
 # INSTALL COMPOSER DEPENDENCIES
-echo "Installing Composer dependencies..."
+echo "=== Installing Composer dependencies ==="
 $PHP_BIN composer.phar install --no-interaction --no-dev --optimize-autoloader
 
 # GENERATE APP KEY
-echo "Generating app key..."
+echo "=== Generating app key ==="
 $PHP_BIN artisan key:generate
 
 # BUILD STATIC SITE
-echo "Warming stache..."
+echo "=== Warming stache ==="
 $PHP_BIN please stache:warm -n -q
-echo "Generating static site..."
+echo "=== Generating static site ==="
 $PHP_BIN please ssg:generate
+
+echo "=== Build complete ==="
