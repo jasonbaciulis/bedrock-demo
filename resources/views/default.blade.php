94ee6d655e661d8268b8081b91be378da0f73e96
Jason Baciulis <jason@remarkable.dev>
2026-05-28 21:08:36 +0700

    feat: convert to antlers

--- resources/views/default.blade.php
+++
@@ -1,9 +0,0 @@
-@extends ('layout')
-
-@section ('body')
-    <main id="content">
-        @foreach ($page->blocks as $block)
-            @include ('blocks.' . str_replace('_', '-', $block->type), ['block' => $block])
-        @endforeach
-    </main>
-@endsection
