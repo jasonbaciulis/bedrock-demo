db0d1c00de403523da6f4cc92548ddd416dab579
Jason Baciulis <jason@remarkable.dev>
2026-05-29 18:17:49 +0700

    feat: merge custom composer scripts

--- package/StarterKitPostInstall.php
+++ package/StarterKitPostInstall.php
@@ -4,11 +4,59 @@ use function Laravel\Prompts\{confirm, info};
 
 class StarterKitPostInstall
 {
-    public function handle($console)
+    public function handle($console): void
     {
         info('Thanks for installing Bedrock starter kit!');
 
+        $this->mergeComposerScripts();
+
         $this->starRepo();
+    }
+
+    protected function mergeComposerScripts(): void
+    {
+        $path = getcwd().'/composer.json';
+
+        $composer = json_decode(file_get_contents($path), true);
+
+        $composer['scripts'] = array_merge($composer['scripts'] ?? [], $this->customScripts());
+
+        file_put_contents(
+            $path,
+            json_encode($composer, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE).PHP_EOL
+        );
+
+        info('Added Bedrock composer scripts.');
+    }
+
+    /**
+     * @return array<string, string|array<int, string>>
+     */
+    protected function customScripts(): array
+    {
+        return [
+            'dev' => [
+                'Composer\\Config::disableProcessTimeout',
+                'npx concurrently -c "#c4b5fd,#fb7185,#fdba74" "php artisan queue:listen --tries=1" "php artisan pail --timeout=0" "npm run dev" --names=queue,logs,vite',
+            ],
+            'pint' => 'pint',
+            'pint:ci' => 'pint --test --parallel',
+            'phpstan' => 'phpstan analyse --configuration=phpstan.neon --no-progress',
+            'phpstan:ci' => 'phpstan analyse --configuration=phpstan.neon --no-progress --no-interaction',
+            'format' => [
+                'npm run lint',
+                'npm run format',
+                '@pint',
+                '@phpstan',
+            ],
+            'format:ci' => [
+                '@pint:ci',
+                '@phpstan:ci',
+            ],
+            'test' => [
+                'pest --parallel',
+            ],
+        ];
     }
 
     protected function starRepo(): void
